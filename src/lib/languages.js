f987138cfeb58de5d822d1e45e14548095694651
MumnaBe <mumna.begum@hc-sc.gc.ca>
2025-04-24 11:21:13 -0400

    Feature/dht 1455 (#17)
    * Disclaimer info.
    
    * Disclaimer edits
    
    * Updated scan information
    
    * Validation translations

--- src/lib/languages.js
+++ src/lib/languages.js
@@ -92,7 +92,7 @@ export const languages = {
     scanError: 'Error scanning QR code. Please try again.',
     scanSuccess: 'QR code scanned successfully',
     readCode: 'Read Code',
-
+    scanTitle: 'Scan a Smart Health Card QR Code',
     // Buttons
     saveToPDF: 'Save to PDF',
     saveToFHIR: 'Save to FHIR',
@@ -104,11 +104,19 @@ export const languages = {
     // TCPFooter.js
     disclaimer: 'DISCLAIMER:',
     disclaimerDescription: 'THE COMMONS PROJECT FOUNDATION (“TCP”) DOES NOT PROVIDE MEDICAL ADVICE OR ADMINISTER ANY DIAGNOSTIC MEDICAL TESTS, VACCINES OR OTHER HEALTHCARE INTERVENTIONS. TCP MAKES NO ENDORSEMENT OR REPRESENTATION AS TO THE ACCREDITATION, LICENSING OR GOOD-STANDING OF ANY HEALTHCARE PROVIDER UNDER APPLICABLE STATE, FEDERAL, NATIONAL OR SUPRANATIONAL LAWS AND REGULATIONS. TCP EXPRESSLY DISCLAIMS ANY AND ALL LIABILITY FOR ANY CONSEQUENTIAL, INDIRECT, INCIDENTAL, SPECIAL OR EXEMPLARY DAMAGES, INCLUDING WITHOUT LIMITATION ANY LOSS OF REVENUES OR PROFITS OR ANY LOSS OF USE OF DATA, ARISING OUT OF OR CONNECTED IN ANY WAY WITH ANY DIAGNOSTIC TESTING, MEDICAL TREATMENT, VACCINATION OR OTHER HEALTHCARE INTERVENTION OR OTHERWISE PROVIDED, SPONSORED, OR PROMOTED BY ANY CTN MEMBER, WHETHER PROVIDED BY THE MEMBER ITSELF OR BY ITS AFFILIATES, REPRESENTATIVES, AGENTS OR SUBCONTRACTORS.',
+    linksDislaimer: ' ',
     privacyPolicy: 'Website Privacy Policy',
     termsOfService: 'Terms of Service',
     
     // About.js
     about: 'About',
+
+    // ValidationInfo.js
+    validation1: 'This card is',
+    validation2: 'valid',
+    validation3: 'and was issued by',
+    validation4: 'on',
+
   },
   fr: {
     // App.js
@@ -130,17 +138,17 @@ export const languages = {
     aboutContent1:'The Commons Project',
     aboutContent2:', cette ',
     aboutContent3: 'application open source',
-    aboutContent4: ' peut être utilisée de manière autonome ou intégrée dans un DSE pour lire les informations contenues dans ',
-    aboutContent5:'les cartes de santé et les liens SMART.',
-    aboutContent6:' Les types de données pris en charge comprennent actuellement ', 
-    aboutContent7:'les cartes de vaccination contre la COVID-19,',
-    aboutContent8:' les dossiers généraux de vaccination, ', 
-    aboutContent9:'les résumés internationaux de dossiers de patients,', 
+    aboutContent4: ' peut être utilisée de manière autonome ou intégrée dans un DSE pour lire les informations contenues dans les ',
+    aboutContent5:' cartes de santé et les liens SMART.',
+    aboutContent6:' Les types de données pris en charge comprennent actuellement les ', 
+    aboutContent7:' cartes de vaccination contre la COVID-19,',
+    aboutContent8:' les dossiers généraux de vaccination, les ', 
+    aboutContent9:' résumés internationaux de dossiers de patients,', 
     aboutContent10: " et les cartes numériques d'assurance maladie. ",
     aboutContact1:'The Commons Project.',
     aboutContact: "Si vous souhaitez héberger vous-même le lecteur, apporter des fonctionnalités ou des correctifs au projet, ou si vous avez d'autres questions, veuillez contacter ",
     aboutPrivacy: 'Les informations personnelles relatives à la santé sont traitées exclusivement dans le navigateur et ne sont jamais envoyées aux serveurs hébergeant le lecteur.',
-
+    
 
     // Photo.js
     captureTitle: "Capturer l'image du code QR de santé Smart",
@@ -203,6 +211,7 @@ export const languages = {
     scanError: 'Erreur lors du scan du code QR. Veuillez réessayer.',
     scanSuccess: 'Code QR scanné avec succès',
     readCode: 'Lire le code',
+    scanTitle: 'Numériser un code QR de santé Smart',
 
     // Buttons
     saveToPDF: 'Sauvegarder au Format PDF',
@@ -213,11 +222,22 @@ export const languages = {
     openFileText: 'Ouvrir le fichier',
 
     // TCPFooter.js
-    disclaimer: 'DISCLAIMER:',
-    disclaimerDescription: "CLAUSE DE NON-RESPONSABILITÉ : LA FONDATION COMMONS PROJECT (« TCP ») NE FOURNIT PAS DE CONSEILS MÉDICAUX ET N'ADMINISTRE PAS DE TESTS MÉDICAUX DIAGNOSTIQUES, DE VACCINS OU D'AUTRES INTERVENTIONS DE SANTÉ. LA TCP NE DONNE AUCUNE GARANTIE NI NE FAIT AUCUNE DÉCLARATION QUANT À L'ACCRÉDITATION, L'AUTORISATION D'EXERCER OU LA BONNE RÉPUTATION D'UN PRESTATAIRE DE SOINS DE SANTÉ EN VERTU DES LOIS ET RÉGLEMENTATIONS ÉTATIQUES, FÉDÉRALES, NATIONALES OU SUPRANATIONALES EN VIGUEUR. LE TCP DÉCLINE EXPRESSÉMENT TOUTE RESPONSABILITÉ EN CAS DE DOMMAGES CONSÉCUTIFS, INDIRECTS, ACCESSOIRES, SPÉCIAUX OU EXEMPLAIRES, Y COMPRIS, MAIS SANS S'Y LIMITER, TOUTE PERTE DE REVENUS OU DE BÉNÉFICES OU TOUTE PERTE D'UTILISATION DE DONNÉES, DÉCOULANT DE OU LIÉS DE QUELQUE MANIÈRE QUE CE SOIT À TOUT TEST DE DIAGNOSTIC, TRAITEMENT MÉDICAL, VACCINATION OU AUTRE INTERVENTION DE SOINS DE SANTÉ OU AUTREMENT FOURNI, PARRAINÉ OU PROMU PAR UN MEMBRE DU CTN, QU'IL SOIT FOURNI PAR LE MEMBRE LUI-MÊME OU PAR SES AFFILIÉS, SES REPRÉSENTANTS, SES AGENTS OU SES SOUS-TRAITANTS.",
+    disclaimer: 'CLAUSE DE NON-RESPONSABILITÉ:',
+    disclaimerDescription: "LA FONDATION COMMONS PROJECT (« TCP ») NE FOURNIT PAS DE CONSEILS MÉDICAUX ET N'ADMINISTRE PAS DE TESTS MÉDICAUX DIAGNOSTIQUES, DE VACCINS OU D'AUTRES INTERVENTIONS DE SANTÉ. LA TCP NE DONNE AUCUNE GARANTIE NI NE FAIT AUCUNE DÉCLARATION QUANT À L'ACCRÉDITATION, L'AUTORISATION D'EXERCER OU LA BONNE RÉPUTATION D'UN PRESTATAIRE DE SOINS DE SANTÉ EN VERTU DES LOIS ET RÉGLEMENTATIONS ÉTATIQUES, FÉDÉRALES, NATIONALES OU SUPRANATIONALES EN VIGUEUR. LE TCP DÉCLINE EXPRESSÉMENT TOUTE RESPONSABILITÉ EN CAS DE DOMMAGES CONSÉCUTIFS, INDIRECTS, ACCESSOIRES, SPÉCIAUX OU EXEMPLAIRES, Y COMPRIS, MAIS SANS S'Y LIMITER, TOUTE PERTE DE REVENUS OU DE BÉNÉFICES OU TOUTE PERTE D'UTILISATION DE DONNÉES, DÉCOULANT DE OU LIÉS DE QUELQUE MANIÈRE QUE CE SOIT À TOUT TEST DE DIAGNOSTIC, TRAITEMENT MÉDICAL, VACCINATION OU AUTRE INTERVENTION DE SOINS DE SANTÉ OU AUTREMENT FOURNI, PARRAINÉ OU PROMU PAR UN MEMBRE DU CTN, QU'IL SOIT FOURNI PAR LE MEMBRE LUI-MÊME OU PAR SES AFFILIÉS, SES REPRÉSENTANTS, SES AGENTS OU SES SOUS-TRAITANTS.",
     privacyPolicy: 'Politique sur la vie privée du site internet',
     termsOfService: 'Termes et conditions',
+    linksDislaimer: '*Tous les liens sont disponibles en anglais seulement ',
+
     // About.js
     about: 'À propos',
+
+    //Validation.js
+    validation1: 'Cette carte est',
+    validation2: 'valide',
+    validation3: 'et a été émise par le',
+    validation4: 'le',
+
+
+
   }
 }; 

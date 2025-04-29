9402487aca7fa187b244ae7f81c03f60db92dc36
MumnaBe <mumna.begum@hc-sc.gc.ca>
2025-04-29 14:35:00 -0400

    Reverted scroll features

--- src/App.js
+++ src/App.js
@@ -52,8 +52,7 @@ function AppContent() {
           onChange={handleTabChange}
           orientation='horizontal'
           variant='scrollable'
-          scrollButtons="auto"
-          allowScrollButtonsMobile
+
         >
           <Tab label={t('aboutTab')} value={TabValue.About} />
           {config("showFile") && <Tab label={t('fileTab')} value={TabValue.File} />}
